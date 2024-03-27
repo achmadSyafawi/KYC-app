7d6d93c8110bd1c0e8e6737d9a9988c9a88e0281
Achmad Syifau Syafawi <achmadsyifausyafawi@Achmads-MacBook-Pro-2.local>
2024-03-27 23:30:04 +0700

    modify json config and next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // basePath: "/kyc-app",
-  output: "export", //enable export
+  // output: "export", //enable export
+  output: "standalone",
   reactStrictMode: true,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
