bcc3cc0214a39d5352f445603ddb807dbf994872
Achmad Syifau Syafawi <achmadsyifausyafawi@Achmads-MacBook-Pro-2.local>
2024-03-28 00:22:52 +0700

    change version

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   // basePath: "/kyc-app",
   // output: "export", //enable export
   output: "standalone",
+  distDir: "dir",
   reactStrictMode: true,
   trailingSlash: true,
 };
