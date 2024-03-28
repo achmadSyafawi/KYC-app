bd3a653d8f269f6e05df70c682da6d15573f7efb
Achmad Syifau Syafawi <achmadsyifausyafawi@Achmads-MacBook-Pro-2.local>
2024-03-29 03:13:02 +0700

    modify redirect

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // basePath: "/kyc-app",
-  output: "export", //enable export
-  // output: "standalone",
-  distDir: "./dist",
+  // output: "export", //enable export
+  output: "standalone",
   reactStrictMode: true,
   trailingSlash: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `https://api.vision.glair.ai/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/component/utils/index.js
+++ src/component/utils/index.js
@@ -10,8 +10,10 @@ export const senApi = async ({ img, type }) => {
     "Basic " + Buffer.from(`${username}:${password}`).toString("base64");
   const apiKey = apiKey1;
   const data = new FormData();
-  data.set("file", img);
-  data.set("type", type);
+  let url = "";
+  // data.set("file", img);
+  // data.set("type", type);
+  data.set("image", img);
   const config = {
     method: "POST",
     headers: {
@@ -21,7 +23,24 @@ export const senApi = async ({ img, type }) => {
     body: data,
   };
 
-  const response = await fetch("/api/upload", config);
+  if (type === "ktp") {
+    url = `/ocr/v1/ktp`;
+  } else if (type === "npwp") {
+    url = `/ocr/v1/npwp`;
+  } else if (type === "sim") {
+    url = `/ocr/v1/kk`;
+  } else {
+    url = `/ocr/v1/sim`;
+  }
 
-  return response.json();
+  try {
+    const response = await fetch(url, config);
+    const responseData = await response.json();
+
+    // console.log(responseData);
+
+    return responseData;
+  } catch (err) {
+    return { success: false, errMsg: err.message };
+  }
 };
